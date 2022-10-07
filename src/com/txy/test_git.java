666d78568f76867d341d69b6da99c56bf63bfcd6
txy6666yr <1764646149@qq.com>
2022-10-07 21:19:20 +0800

    v2

--- src/com/txy/test_git.java
+++ src/com/txy/test_git.java
@@ -3,5 +3,6 @@ package com.txy;
 public class test_git {
     public static void main(String[] args) {
         System.out.println("hello java v1");
+        System.out.println("hello java v2");
     }
 }
