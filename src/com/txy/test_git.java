5c9f51791086c32398581cf680d833ebbeacde27
txy6666yr <1764646149@qq.com>
2022-10-08 12:02:25 +0800

    bugfix branch

--- src/com/txy/test_git.java
+++ src/com/txy/test_git.java
@@ -6,5 +6,6 @@ public class test_git {
         System.out.println("hello java v2");
         System.out.println("v3");
         System.out.println("现在是在bug-fix分支 modify");
+        System.out.println("对应 现在是bug-fix分支 modify");
     }
 }
